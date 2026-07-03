379356dea4a1fdff2f5c9d140258a6a0d47e6c7e
Emanuel Soto <50783299+EmaSleal@users.noreply.github.com>
2026-07-03 09:06:10 -0600

    feat(workout-plan): render workout plan detail grouped by daily routine

--- src/app/pages/routine/workout-plan/routine.component.ts
+++ src/app/pages/routine/workout-plan/routine.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { WorkoutPlanService } from '../../../core/service/workout-plan/workout-plan.service';
 import { WorkoutPlan } from '../../../core/models/workout-plan.interface';
+import { CardComponent } from '../../../utils/card/card.component';
 
 @Component({
   selector: 'app-routine',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CardComponent],
   templateUrl: './routine.component.html',
   styleUrl: './routine.component.scss'
 })
 export class RoutineComponent implements OnInit {
   workoutPlan: WorkoutPlan | undefined;
+  notFound = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -24,7 +25,9 @@ export class RoutineComponent implements OnInit {
     this.workoutPlanService.findById(id).subscribe({
       next: (workoutPlan) => {
         this.workoutPlan = workoutPlan;
-        console.log(this.workoutPlan);
+      },
+      error: () => {
+        this.notFound = true;
       }
     });
   }
