370a9652217e17c59cbf1c3e6dc205e77b62e49f
Emanuel Soto <50783299+EmaSleal@users.noreply.github.com>
2026-07-02 22:03:54 -0600

    chore(build): remove Angular SSR/server-side rendering setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,6 @@ import { provideRouter } from '@angular/router';
 import { provideServiceWorker } from '@angular/service-worker';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { AuthInterceptorFn } from './core/interceptors/authInterceptor';
@@ -13,7 +12,6 @@ import { DialogService } from 'primeng/dynamicdialog';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideClientHydration(),
     provideAnimationsAsync(),
     provideHttpClient(
       withFetch(),
