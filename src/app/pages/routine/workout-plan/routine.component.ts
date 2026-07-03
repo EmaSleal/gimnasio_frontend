951e48fa99c71dc209a891e1f634a106aabeadc2
Emanuel Soto <50783299+EmaSleal@users.noreply.github.com>
2026-07-02 23:23:37 -0600

    feat(workout-plan): fetch and display workout plan by id in detail route

--- src/app/pages/routine/workout-plan/routine.component.ts
+++ src/app/pages/routine/workout-plan/routine.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { WorkoutPlanService } from '../../../core/service/workout-plan/workout-plan.service';
+import { WorkoutPlan } from '../../../core/models/workout-plan.interface';
 
 @Component({
   selector: 'app-routine',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './routine.component.html',
   styleUrl: './routine.component.scss'
 })
-export class RoutineComponent {
+export class RoutineComponent implements OnInit {
+  workoutPlan: WorkoutPlan | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private workoutPlanService: WorkoutPlanService
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.workoutPlanService.findById(id).subscribe({
+      next: (workoutPlan) => {
+        this.workoutPlan = workoutPlan;
+        console.log(this.workoutPlan);
+      }
+    });
+  }
 }
